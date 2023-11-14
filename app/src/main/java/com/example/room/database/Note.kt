89ede4051af420466e1a384e67ce0132dd919d28
Vympel87 <111189503+Vympel87@users.noreply.github.com>
2023-11-14 19:55:12 +0700

    Initial commit

---
+++ app/src/main/java/com/example/room/database/Note.kt
@@ -0,0 +1,26 @@
+package com.example.room.database
+
+import android.accounts.AuthenticatorDescription
+import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.lang.reflect.Constructor
+
+@Entity(tableName = "note_table")
+data class Note(
+    @PrimaryKey(autoGenerate = true)
+    @NonNull
+    val id: Int=0,
+    @ColumnInfo(name = "title")
+    val title: String,
+    @ColumnInfo(name = "description")
+    val description: String,
+    @ColumnInfo(name = "date")
+    val date: String
+
+//    constructor(val title: String, val description: String, date: String) {
+//
+//    }
+)
+
